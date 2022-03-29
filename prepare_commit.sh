d86ce6ee6320a81494a417c805c06867dba88f64
Lanfranchini <gregoire@myem.fr>
2022-03-29 15:04:41 +0200

    "$JIRA_BRANCH_IDENTIFIER" #time "$TIME" #comment "$MESSAGE"

--- prepare_commit.sh
+++ prepare_commit.sh
@@ -8,5 +8,5 @@ read -p "Was it long ? Like how long ? (Format is 'h' for hour and 'm' for minut
 echo ${TIME}
 read -p "That much ?? What did you do all this time (commit message): " MESSAGE
 echo ${MESSAGE}
-FORMATTED_MESSAGE='$JIRA_BRANCH_IDENTIFIER #time $TIME #comment $MESSAGE'
+FORMATTED_MESSAGE='"$JIRA_BRANCH_IDENTIFIER" #time "$TIME" #comment "$MESSAGE"'
 ${GIT} commit -m "$FORMATTED_MESSAGE"
