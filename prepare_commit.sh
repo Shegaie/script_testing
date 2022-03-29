2e8caf76c881c022f93813e138cfac67c46fa45e
Lanfranchini <gregoire@myem.fr>
2022-03-29 14:58:19 +0200

    "JIRA_BRANCH_IDENTIFIER" #time "TIME" #comment "MESSAGE"

---
+++ prepare_commit.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+GIT=`which git`
+REPO_DIR=$PWD
+cd ${REPO_DIR}
+JIRE_BRANCH_IDENTIFIER=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+read -p "Was it long ? Like how long ? (Format is 'h' for hour and 'm' for minutes): " TIME
+echo ${TIME}
+read -p "That much ?? What did you do all this time (commit message): " MESSAGE
+echo ${MESSAGE}
+echo '"JIRA_BRANCH_IDENTIFIER" #time "TIME" #comment "MESSAGE"'
+${GIT} commit -m '"JIRA_BRANCH_IDENTIFIER" #time "TIME" #comment "MESSAGE"'
