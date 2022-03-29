1bd970209e2a6a125c08e35b887b76dab2d7bd6d
Lanfranchini <gregoire@myem.fr>
2022-03-29 15:07:46 +0200

    MYEM-1598 #time 1h 20m #comment V2

--- prepare_commit.sh
+++ prepare_commit.sh
@@ -3,7 +3,8 @@
 GIT=`which git`
 REPO_DIR=$PWD
 cd ${REPO_DIR}
-JIRE_BRANCH_IDENTIFIER=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+JIRA_BRANCH_IDENTIFIER=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+echo $JIRA_BRANCH_IDENTIFIER
 read -p "Was it long ? Like how long ? (Format is 'h' for hour and 'm' for minutes): " TIME
 echo ${TIME}
 read -p "That much ?? What did you do all this time (commit message): " MESSAGE
