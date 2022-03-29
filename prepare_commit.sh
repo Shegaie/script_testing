f4d52c7ff85d7951f64bd5e6138809b13c04f66a
Lanfranchini <gregoire@myem.fr>
2022-03-29 16:10:36 +0200

    main #time  #comment

--- prepare_commit.sh
+++ prepare_commit.sh
@@ -3,11 +3,30 @@
 GIT=`which git`
 REPO_DIR=$PWD
 cd ${REPO_DIR}
-JIRA_BRANCH_IDENTIFIER=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
-echo $JIRA_BRANCH_IDENTIFIER
-read -p "Was it long ? Like how long ? (Format is 'h' for hour and 'm' for minutes): " TIME
-echo ${TIME}
-read -p "That much ?? What did you do all this time (commit message): " MESSAGE
-echo ${MESSAGE}
-FORMATTED_MESSAGE=$JIRA_BRANCH_IDENTIFIER" #time "$TIME" #comment "$MESSAGE
-${GIT} commit -m "$FORMATTED_MESSAGE"
+if [ ! -z $LAST_COMMIT ]; then
+    read -p "I found that you made this commit earlier: "$LAST_COMMIT"\n\n Would you like to reuse it ? (y/n)"
+    if [ "$answer" != "${answer#[Yy]}" ] ;then
+	eval $LAST_COMMIT
+	exit 0
+    else
+	JIRA_BRANCH_IDENTIFIER=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+	echo $JIRA_BRANCH_IDENTIFIER
+	read -p "Was it long ? Like how long ? (Format is 'h' for hour and 'm' for minutes): " TIME
+	echo ${TIME}
+	read -p "That much ?? What did you do all this time (commit message): " MESSAGE
+	echo ${MESSAGE}
+	FORMATTED_MESSAGE=$JIRA_BRANCH_IDENTIFIER" #time "$TIME" #comment "$MESSAGE
+	export LAST_COMMIT='$GIT commit -m "$FORMATTED_MESSAGE"'
+	${GIT} commit -m "$FORMATTED_MESSAGE"
+    fi
+else
+    JIRA_BRANCH_IDENTIFIER=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+    echo $JIRA_BRANCH_IDENTIFIER
+    read -p "Was it long ? Like how long ? (Format is 'h' for hour and 'm' for minutes): " TIME
+    echo ${TIME}
+    read -p "That much ?? What did you do all this time (commit message): " MESSAGE
+    echo ${MESSAGE}
+    FORMATTED_MESSAGE=$JIRA_BRANCH_IDENTIFIER" #time "$TIME" #comment "$MESSAGE
+    export LAST_COMMIT='$GIT commit -m "$FORMATTED_MESSAGE"'
+    ${GIT} commit -m "$FORMATTED_MESSAGE"
+fi
