d2fb762c657c76fccb096c81a0f0ccc0ffdcc4e5
Lanfranchini <gregoire@myem.fr>
2022-03-30 15:36:25 +0200

    main #time 1h 30m #comment Testing things

--- prepare_commit.sh
+++ prepare_commit.sh
@@ -3,11 +3,21 @@
 GIT=`which git`
 REPO_DIR=$PWD
 cd ${REPO_DIR}
+
+#Get jira branch identifier from your current branch
 JIRA_BRANCH_IDENTIFIER=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
-echo $JIRA_BRANCH_IDENTIFIER
-read -p "Was it long ? Like how long ? (Format is 'h' for hour and 'm' for minutes): " TIME
+echo "Jira branch identifier will be :"$JIRA_BRANCH_IDENTIFIER"\n"
+
+#Ask you the time you spent on this commit
+LAST_TIME=$(sed '1q;d' /tmp/commit_params_history.txt);
+read -p "Was it long ? Like how long ? (Format is 'h' for hour and 'm' for minutes) (default: "$LAST_TIME"): " TIME
 echo ${TIME} > /tmp/commit_params_history.txt
-read -p "That much ?? What did you do all this time (commit message): " MESSAGE
+
+#Ask what you did on this commit                                                                                                                                                                        
+LAST_MESSAGE=$(sed '2q;d' /tmp/commit_params_history.txt);
+read -p "That much ?? What did you do all this time (commit message) (default: "$LAST_MESSAGE"): " MESSAGE
 echo ${MESSAGE} >> /tmp/commit_params_history.txt
+
+#Build the command and fire it
 FORMATTED_MESSAGE=$JIRA_BRANCH_IDENTIFIER" #time "$TIME" #comment "$MESSAGE
 ${GIT} commit -m "$FORMATTED_MESSAGE"
