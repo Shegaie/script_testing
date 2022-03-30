f933c8445f45914b8bff87419af938bd15029a7b
Lanfranchini <gregoire@myem.fr>
2022-03-30 10:59:34 +0200

    main #time 10m #comment Set var in tmp file for history

--- prepare_commit.sh
+++ prepare_commit.sh
@@ -3,30 +3,13 @@
 GIT=`which git`
 REPO_DIR=$PWD
 cd ${REPO_DIR}
-if [ ! -z $LAST_COMMIT ]; then
-    read -p "I found that you made this commit earlier: "$LAST_COMMIT"\n\n Would you like to reuse it ? (y/n)"
-    if [ "$answer" != "${answer#[Yy]}" ] ;then
-	eval $LAST_COMMIT
-	exit 0
-    else
-	JIRA_BRANCH_IDENTIFIER=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
-	echo $JIRA_BRANCH_IDENTIFIER
-	read -p "Was it long ? Like how long ? (Format is 'h' for hour and 'm' for minutes): " TIME
-	echo ${TIME}
-	read -p "That much ?? What did you do all this time (commit message): " MESSAGE
-	echo ${MESSAGE}
-	FORMATTED_MESSAGE=$JIRA_BRANCH_IDENTIFIER" #time "$TIME" #comment "$MESSAGE
-	export LAST_COMMIT='$GIT commit -m "$FORMATTED_MESSAGE"'
-	${GIT} commit -m "$FORMATTED_MESSAGE"
-    fi
-else
-    JIRA_BRANCH_IDENTIFIER=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
-    echo $JIRA_BRANCH_IDENTIFIER
-    read -p "Was it long ? Like how long ? (Format is 'h' for hour and 'm' for minutes): " TIME
-    echo ${TIME}
-    read -p "That much ?? What did you do all this time (commit message): " MESSAGE
-    echo ${MESSAGE}
-    FORMATTED_MESSAGE=$JIRA_BRANCH_IDENTIFIER" #time "$TIME" #comment "$MESSAGE
-    export LAST_COMMIT='$GIT commit -m "$FORMATTED_MESSAGE"'
-    ${GIT} commit -m "$FORMATTED_MESSAGE"
-fi
+JIRA_BRANCH_IDENTIFIER=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+echo $JIRA_BRANCH_IDENTIFIER
+read -p "Was it long ? Like how long ? (Format is 'h' for hour and 'm' for minutes): " TIME
+echo ${TIME} > /tmp/commit_params_history.txt
+echo "\n" > /tmp/commit_params_history.txt
+read -p "That much ?? What did you do all this time (commit message): " MESSAGE
+echo ${MESSAGE} >> /tmp/commit_params_history.txt
+echo "\n" > /tmp/commit_params_history.txt
+FORMATTED_MESSAGE=$JIRA_BRANCH_IDENTIFIER" #time "$TIME" #comment "$MESSAGE
+${GIT} commit -m "$FORMATTED_MESSAGE"
