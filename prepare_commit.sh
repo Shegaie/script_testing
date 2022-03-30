a4914bcd3020d8fed6fa4b1dbbc9f8678ca137b7
Lanfranchini <gregoire@myem.fr>
2022-03-30 11:01:36 +0200

    main #time 1m #comment remove

--- prepare_commit.sh
+++ prepare_commit.sh
@@ -7,9 +7,7 @@ JIRA_BRANCH_IDENTIFIER=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 echo $JIRA_BRANCH_IDENTIFIER
 read -p "Was it long ? Like how long ? (Format is 'h' for hour and 'm' for minutes): " TIME
 echo ${TIME} > /tmp/commit_params_history.txt
-echo "\n" >> /tmp/commit_params_history.txt
 read -p "That much ?? What did you do all this time (commit message): " MESSAGE
 echo ${MESSAGE} >> /tmp/commit_params_history.txt
-echo "\n" >> /tmp/commit_params_history.txt
 FORMATTED_MESSAGE=$JIRA_BRANCH_IDENTIFIER" #time "$TIME" #comment "$MESSAGE
 ${GIT} commit -m "$FORMATTED_MESSAGE"
