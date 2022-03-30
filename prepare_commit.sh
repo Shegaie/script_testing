afb186b0d2aeeb657e6fe90461ca8198cc75c715
Lanfranchini <gregoire@myem.fr>
2022-03-30 17:31:05 +0200

    main #time 5m #comment Solve bug

--- prepare_commit.sh
+++ prepare_commit.sh
@@ -12,22 +12,27 @@ cd ${REPO_DIR}
 
 #Get jira branch identifier from your current branch
 JIRA_BRANCH_IDENTIFIER=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
-echo "\n${YELLOW}Jira branch identifier will be : ${RED}"$JIRA_BRANCH_IDENTIFIER
+echo "\n${YELLOW}Jira branch identifier will be => ${RED}"$JIRA_BRANCH_IDENTIFIER
 
 #Ask you the time you spent on this commit
 LAST_TIME=$(sed '1q;d' /tmp/commit_params_history.txt);
 echo "${BLUE}Was it long ? Like how long ? (Format is 'h' for hour and 'm' for minutes) ${GREEN}(default: "$LAST_TIME"): ${RED}\c"
 read TIME
 if test -z ${TIME}; then TIME=$LAST_TIME; else echo ${TIME} > /tmp/commit_params_history.txt; fi
-echo "${YELLOW}Ok then time will be: ${RED}"$TIME
+echo "${YELLOW}Ok then time will be => ${RED}"$TIME
 
 #Ask what you did on this commit
 LAST_MESSAGE=$(sed '2q;d' /tmp/commit_params_history.txt);
 echo "${BLUE}That much ?? What did you do all this time (commit message) ${GREEN}(default: "$LAST_MESSAGE"): ${RED}\c"
 read MESSAGE
 echo ${MESSAGE} >> /tmp/commit_params_history.txt
-if test -z ${MESSAGE}; then MESSAGE=${LAST_MESSAGE}; else echo ${MESSAGE} >> /tmp/commit_params_history.txt; fi
-echo "${YELLOW}Ok then message will be: ${RED}"$MESSAGE
+
+if test -z ${MESSAGE};
+then MESSAGE=$LAST_MESSAGE;
+else echo ${MESSAGE} >> /tmp/commit_params_history.txt;
+fi
+
+echo "${YELLOW}Ok then message will be => ${RED}"$MESSAGE
 
 #Build the command and fire it
 echo "${NC}"
